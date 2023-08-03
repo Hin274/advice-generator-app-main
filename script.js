1f82cb3e6729ab39d6caed7f389177acf074432e
ChongHinYu <mailtohin274@hotmail.com>
2023-08-03 16:40:34 +0100

    Added loading text and function to get new advice

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-fetch("https://api.adviceslip.com/advice")
-    .then((response) => {
-        if (response.ok) {
-            return response.json()
-        }
-        else {
-            throw new Error("Network response error")
-        }
-    })
-    .then(data => {
-        console.log(data)
-        displayQuote(data)
-    })
-    .catch((error) => console.error("Fetch Error:", error))
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
 
-function displayQuote(data) {
+displayQuote = (data) => {
     const adviceIDDiv = document.getElementById("adviceID")
     const id = data.slip.id
-    const adviceID = document.createElement("p")
-    adviceID.innerHTML = `ADVICE #${id}`
-    adviceIDDiv.appendChild(adviceID)
-
     const adviceDiv = document.getElementById("advice")
 
-    const advice = data.slip.advice
-    const adviceText = document.createElement("p")
-    adviceText.innerHTML = `"${advice}"`
-    adviceDiv.appendChild(adviceText)
+    createQuote = () => {
+        const adviceID = document.createElement("p")
+        adviceID.innerHTML = `ADVICE #${id}`
+        adviceIDDiv.appendChild(adviceID)
+        const advice = data.slip.advice
+        const adviceText = document.createElement("p")
+        adviceText.innerHTML = `"${advice}"`
+        adviceDiv.appendChild(adviceText)
+    }
+    if (adviceIDDiv.hasChildNodes() & adviceDiv.hasChildNodes()) {
+        adviceIDDiv.removeChild(adviceIDDiv.firstElementChild)
+        adviceDiv.removeChild(adviceDiv.firstElementChild)
+        createQuote()
+    } else {
+        createQuote()
+    }
 
 }
+
+getQuote = () => {
+    fetch("https://api.adviceslip.com/advice")
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            else {
+                throw new Error("Network response error")
+            }
+        })
+        .then(data => {
+            console.log(data)
+            hideloader()
+            displayQuote(data)
+        })
+        .catch((error) => console.error("Fetch Error:", error))
+}
+
+getQuote()
